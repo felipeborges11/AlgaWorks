67da1e36b86be5013c32e995e0e2b841a44f5100
Felipe Borges da Silva <felipebsilva5@gmail.com>
2020-10-27 00:33:39 -0300

    UPDATE: ClientController

--- src/main/java/com/felipe/osworks/osworksapi/controller/ClientController.java
+++ src/main/java/com/felipe/osworks/osworksapi/controller/ClientController.java
@@ -4,8 +4,6 @@ import com.felipe.osworks.osworksapi.domain.model.Client;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
