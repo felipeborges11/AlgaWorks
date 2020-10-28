549f2d5c4c4c53e8647fb1eeebb65bcfd35e08ea
Felipe Borges da Silva <felipebsilva5@gmail.com>
2020-10-27 23:43:25 -0300

    UPDATE: ClientController CRUD

--- src/main/java/com/felipe/osworks/osworksapi/controller/ClientController.java
+++ src/main/java/com/felipe/osworks/osworksapi/controller/ClientController.java
@@ -1,29 +1,64 @@
 package com.felipe.osworks.osworksapi.controller;
 
 import com.felipe.osworks.osworksapi.domain.model.Client;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.felipe.osworks.osworksapi.domain.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
+import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/clients")
 public class ClientController {
 
-    @GetMapping("/clients")
+    @Autowired
+    private ClientRepository clientRepository;
+
+    @GetMapping
     public List<Client> list(){
-        var client1 = new Client();
-        client1.setId(1L);
-        client1.setName("Felipe");
-        client1.setEmail("felipebsilva5@gmail.com");
-        client1.setPhone("99 99999-9999");
-
-        var client2 = new Client();
-        client2.setId(2L);
-        client2.setName("Borges");
-        client2.setEmail("borges@mail.com");
-        client2.setPhone("88 88888-8888");
-
-        return Arrays.asList(client1,client2);
+        return clientRepository.findAll();
     }
+
+    @GetMapping("/{clientId}")
+    public ResponseEntity<Client> find(@PathVariable Long clientId){
+        Optional<Client>client = clientRepository.findById(clientId);
+        if(client.isPresent()){
+            return ResponseEntity.ok(client.get());
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Client add(@Valid @RequestBody Client client){
+        return clientRepository.save(client);
+    }
+
+    @PutMapping("/{clientId}")
+    public ResponseEntity<Client> update(@Valid @PathVariable Long clientId, @RequestBody Client client){
+        if(!clientRepository.existsById(clientId)){
+           return ResponseEntity.notFound().build();
+        }
+        client.setId(clientId);
+        client = clientRepository.save(client);
+
+        return ResponseEntity.ok(client);
+    }
+
+    @DeleteMapping("/{clientId}")
+    public ResponseEntity<Void> delete(@PathVariable Long clientId){
+        if(!clientRepository.existsById(clientId)){
+            return ResponseEntity.notFound().build();
+        }
+        clientRepository.deleteById(clientId);
+        return ResponseEntity.noContent().build();
+    }
+
+
 }
