4b5d87e47e091878477e0e6f1992225ace736c62
Felipe Borges da Silva <felipebsilva5@gmail.com>
2020-10-27 23:46:04 -0300

    ADD: Client Repository

--- src/main/java/com/felipe/osworks/osworksapi/domain/repository/ClientRepository.java
+++ src/main/java/com/felipe/osworks/osworksapi/domain/repository/ClientRepository.java
@@ -1,2 +1,15 @@
-package com.felipe.osworks.osworksapi.domain.repository;public interface ClientRepository {
+package com.felipe.osworks.osworksapi.domain.repository;
+
+import com.felipe.osworks.osworksapi.domain.model.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
+
+    //List<Client> findByName(String name);
+    //List<Client> findByNameContaining(String name);
+
 }
