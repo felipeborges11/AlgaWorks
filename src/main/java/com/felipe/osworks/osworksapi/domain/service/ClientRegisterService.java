d262dd638dc1571602c110ccb27feef1fcd1fca9
Felipe Borges da Silva <felipebsilva5@gmail.com>
2020-11-05 22:36:53 -0300

    UPDATE: ClientRegisterService

--- src/main/java/com/felipe/osworks/osworksapi/domain/service/ClientRegisterService.java
+++ src/main/java/com/felipe/osworks/osworksapi/domain/service/ClientRegisterService.java
@@ -1,2 +1,29 @@
-package com.felipe.osworks.osworksapi.domain.service;public class ClientRegisterService {
+package com.felipe.osworks.osworksapi.domain.service;
+
+import com.felipe.osworks.osworksapi.domain.exception.BusinessException;
+import com.felipe.osworks.osworksapi.domain.model.Client;
+import com.felipe.osworks.osworksapi.domain.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClientRegisterService {
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    public Client save(Client client) {
+        Client clientExist = clientRepository.findByEmail(client.getEmail());
+
+        if(clientExist != null && !clientExist.equals(client)){
+            throw new BusinessException("Email already registered.");
+        }
+        return clientRepository.save(client);
+    }
+
+    public void delete(Long clientId){
+        clientRepository.deleteById(clientId);
+    }
+
+
 }
