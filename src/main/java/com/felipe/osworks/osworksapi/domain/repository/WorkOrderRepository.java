9b928e04986aa526dd4ecc4c1f8ebfad16cad598
Felipe Borges da Silva <felipebsilva5@gmail.com>
2020-11-07 21:08:41 -0300

    ADD: WorkOrderRepository interface

---
+++ src/main/java/com/felipe/osworks/osworksapi/domain/repository/WorkOrderRepository.java
@@ -0,0 +1,11 @@
+package com.felipe.osworks.osworksapi.domain.repository;
+
+import com.felipe.osworks.osworksapi.domain.model.WorkOrder;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WorkOrderRepository extends JpaRepository<WorkOrder, Long> {
+
+
+}
